95b581a3e1082411fa857de6e015fd89de2807c0
tyl <tyl@tyl.com>
2020-03-01 21:10:16 +0800

    #nfr remove linter warnings

--- task.py
+++ task.py
@@ -5,6 +5,6 @@ def firstrun():
     return "Success"
 
 
-#get the area of a circle
+# get the area of a circle
 def get_area_of_circle(radius):
     return math.pi * radius * radius
--- tests.py
+++ tests.py
@@ -6,8 +6,9 @@ class TestCase(unittest.TestCase):
 
     # example test 1
     def test1(self):
-        #example test 1
+        # example test 1
         expected = "Success"
+
         self.assertEqual(expected, task.firstrun())
 
     # example test 2
@@ -18,9 +19,9 @@ class TestCase(unittest.TestCase):
 
     # test for getting a circle's area from its radius
     def test_circle(self):
-        # get the area of a cicle and also round the answer to 3 decimal places
+        # get the area of a circle and also round the answer to 3 decimal places
         expected = round(task.get_area_of_circle(4.0), 3)
-        #assert and check if they are equal
+        # assert and check if they are equal
         self.assertEqual(50.265, expected)
 
 
