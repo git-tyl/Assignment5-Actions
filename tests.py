1a6247fa0ead6dea8e4a716ce646e4bf12257be6
tyl <tyl@tyl.com>
2020-03-01 21:07:34 +0800

    #nfr remove linter warnings

--- tests.py
+++ tests.py
@@ -4,17 +4,23 @@ import task
 
 class TestCase(unittest.TestCase):
 
+    # example test 1
     def test1(self):
+        #example test 1
         expected = "Success"
         self.assertEqual(expected, task.firstrun())
 
+    # example test 2
     def test2(self):
+        # example test 2
         expected = "Failure"
         self.assertNotEqual(expected, task.firstrun())
 
     # test for getting a circle's area from its radius
     def test_circle(self):
+        # get the area of a cicle and also round the answer to 3 decimal places
         expected = round(task.get_area_of_circle(4.0), 3)
+        #assert and check if they are equal
         self.assertEqual(50.265, expected)
 
 
