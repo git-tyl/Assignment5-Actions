5afc73b74b684c6bef3e03627f3bc8bc750b47d4
tyl <tyl@tyl.com>
2020-03-01 21:05:22 +0800

    #feature get radius

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
+import math
+
+
 def firstrun():
     return "Success"
+
+
+#get the area of a circle
+def get_area_of_circle(radius):
+    return math.pi * radius * radius
--- tests.py
+++ tests.py
@@ -12,6 +12,11 @@ class TestCase(unittest.TestCase):
         expected = "Failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    # test for getting a circle's area from its radius
+    def test_circle(self):
+        expected = round(task.get_area_of_circle(4.0), 3)
+        self.assertEqual(50.265, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
