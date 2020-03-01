26dcd57788318d8f24ed8676fe6a0a0ca1cdacb1
tyl <tyl@tyl.com>
2020-03-01 21:16:00 +0800

    #nfr return initial tests

--- tests.py
+++ tests.py
@@ -4,6 +4,19 @@ import task
 
 class TestCase(unittest.TestCase):
 
+    # example test 1
+    def test1(self):
+        # example test 1
+        expected = "Success"
+
+        self.assertEqual(expected, task.firstrun())
+
+    # example test 2
+    def test2(self):
+        # example test 2
+        expected = "Failure"
+        self.assertNotEqual(expected, task.firstrun())
+
     # test for getting a circle's area from its radius
     def test_circle(self):
         # get the area of a circle and also round the answer to 3 decimal places
